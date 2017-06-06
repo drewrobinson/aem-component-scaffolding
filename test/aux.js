a6105ca9c8d444a62a69d99d5c8dac27875739c6
Douglas Ross <ross@adobe.com>
2017-06-06 12:07:58 -0700

    Added and integrated utility function to adjust for user-specific directory structures; implemented sortByFileName and sortByFileDepth tests

--- test/aux.js
+++ test/aux.js
@@ -1,5 +1,6 @@
-var assert = require('assert');
-var aux    = require('../bin/aux.js');
+const assert      = require('assert');
+const aux         = require('../bin/aux.js');
+const CONTENT_DIR = __dirname + "/content";
 
 describe('Aux', function() {
     describe('#flatten()', function() {
@@ -18,42 +19,88 @@ describe('Aux', function() {
     describe('#crawl()', function() {
         it('Return a list of all the child files and directories under the specified directory without a trailing slash', function() {
 
-            var pageList = [
-                'test/content/component/content/a/.content.xml',
-                'test/content/component/content/a/a.html',
-                'test/content/component/content/b/.content.xml',
-                'test/content/component/content/b/_cq_dialog.xml',
-                'test/content/component/content/b/b.html',
-                'test/content/component/structure/homepage/.content.xml',
-                'test/content/component/structure/page/.content.xml'
+            let expected = [
+                'component/content/.content.xml',
+                'component/content/a/.content.xml',
+                'component/content/a/a.html',
+                'component/content/b/.content.xml',
+                'component/content/b/_cq_dialog.xml',
+                'component/content/b/b.html',
+                'component/structure/.content.xml',
+                'component/structure/homepage/.content.xml',
+                'component/structure/page/.content.xml'
             ];
 
-            assert.equal(aux.crawl(__dirname + "/content").length, pageList.length);
+            assert.deepEqual(expected, reduceTestDirPaths(aux.crawl(CONTENT_DIR)));
         });
         it('Return a list of all the child files and directories under the specified directory with a trailing slash', function() {
-            var pageList = [
-                'test/content/component/content/a/.content.xml',
-                'test/content/component/content/a/a.html',
-                'test/content/component/content/b/.content.xml',
-                'test/content/component/content/b/_cq_dialog.xml',
-                'test/content/component/content/b/b.html',
-                'test/content/component/structure/homepage/.content.xml',
-                'test/content/component/structure/page/.content.xml'
+            let expected = [
+                'component/content/.content.xml',
+                'component/content/a/.content.xml',
+                'component/content/a/a.html',
+                'component/content/b/.content.xml',
+                'component/content/b/_cq_dialog.xml',
+                'component/content/b/b.html',
+                'component/structure/.content.xml',
+                'component/structure/homepage/.content.xml',
+                'component/structure/page/.content.xml'
             ];
 
-            assert.equal(aux.crawl(__dirname + "/content/").length, pageList.length);
+            assert.deepEqual(expected, reduceTestDirPaths(aux.crawl(CONTENT_DIR + "/")));
         });
     });
 
     describe('#sortByFileName()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
+        it('Should return a sorted list based on end file names, regardless of directory structure', function() {
+
+            let expected = [
+                'component/content/.content.xml',
+                'component/content/a/.content.xml',
+                'component/content/b/.content.xml',
+                'component/structure/.content.xml',
+                'component/structure/homepage/.content.xml',
+                'component/structure/page/.content.xml',
+                'component/content/b/_cq_dialog.xml',
+                'component/content/a/a.html',
+                'component/content/b/b.html'
+            ];
+
+            let result = aux.sortByFileName(aux.crawl(CONTENT_DIR));
+            var fileNames = reduceTestDirPaths(result);
+            assert.deepEqual(expected, fileNames);
         });
     });
 
     describe('#sortByFileDepth()', function() {
-        it('should return -1 when the value is not present', function() {
-            assert.equal(-1, [1,2,3].indexOf(4));
+        it('Should return a sorted list based on file depth', function() {
+            var expected = [
+                'component/content/.content.xml',
+                'component/structure/.content.xml',
+                'component/content/a/.content.xml',
+                'component/content/b/.content.xml',
+                'component/structure/homepage/.content.xml',
+                'component/structure/page/.content.xml',
+                'component/content/b/_cq_dialog.xml',
+                'component/content/a/a.html',
+                'component/content/b/b.html'
+            ];
+            var result = aux.sortByFileDepth(aux.sortByFileName(aux.crawl(CONTENT_DIR)));
+            var fileNames = reduceTestDirPaths(result);
+            assert.deepEqual(expected, fileNames);
         });
     });
 });
+
+/**
+ * Utility function to effectively remove user-specific
+ * directory structures considerations from the tests.
+ * @param array the result of running a file traversal or sort operation
+ * @returns an {Array} that starts from the 'component' folder in the test content directory
+ */
+function reduceTestDirPaths(array) {
+    let fileNames = [];
+    for (var i = 0; i < array.length; i++) {
+        fileNames[i] = array[i].substr(array[i].lastIndexOf("component/"), array[i].length);
+    }
+    return fileNames;
+}
