c21ea5373360156d144b6ba4b5ffffb74e02ae4c
drobinso <drobinso@adobe.com>
2019-03-13 23:47:29 -0600

    remove link script

--- bin/link.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node --experimental-modules --no-warnings
-"use strict";
-
-const shell           = require("shelljs");
-
-if (shell.exec('npm link').code !== 0) {
-  shell.echo('Scaffolder linked...');
-  shell.exit(1);
-}
